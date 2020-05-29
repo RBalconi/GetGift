957c653d3a3e7efbbe3f63f4225f7e181ce34786
Rafael Balconi <r_balconi@outlook.com>
2020-05-29 14:48:04 -0400

    Mozilla adaptation

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,24 +1,39 @@
 const intervalDivPoints = setInterval(() => {
-  const divBottomChatTwitch = getElementByXpath("//div[@class='chat-input__buttons-container tw-flex tw-justify-content-between tw-mg-t-1']");
+  const divBottomChatTwitch = getElementByXpath(
+    "//div[@class='chat-input__buttons-container tw-flex tw-justify-content-between tw-mg-t-1']"
+  );
   if (divBottomChatTwitch) {
-    if(!getElementByXpath("//span[@id='span-number-points-receiver']")){
+    if (!getElementByXpath("//span[@id='span-number-points-receiver']")) {
       const divPoints = document.createElement("div");
-      divPoints.classList.add("tw-inline-flex", "tw-relative", "tw-tooltip-wrapper", "revert-position");
+      divPoints.classList.add(
+        "tw-inline-flex",
+        "tw-relative",
+        "tw-tooltip-wrapper",
+        "revert-position"
+      );
       divBottomChatTwitch.appendChild(divPoints);
-      
+
       const button = document.createElement("button");
       button.classList.add("button-points-receiver");
       divPoints.appendChild(button);
 
       const icon = document.createElement("div");
-      icon.classList.add("icon");
+      switch (detectBrowser()) {
+        case "chrome":
+          icon.classList.add("icon", "icon-chrome");
+          break;
+        case "mozilla":
+          icon.classList.add("icon", "icon-mozilla");
+          break;
+      }
       button.appendChild(icon);
-      
+
       const spanNumber = document.createElement("span");
-      if(localStorage.getItem("gg_pointsReceivedToday")){
+      if (localStorage.getItem("gg_pointsReceivedToday")) {
         compareDates();
+        console.log(navigator.userAgent);
         spanNumber.innerHTML = localStorage.getItem("gg_pointsReceivedToday");
-      }else{
+      } else {
         localStorage.setItem("gg_pointsReceivedToday", "00");
         spanNumber.innerHTML = localStorage.getItem("gg_pointsReceivedToday");
       }
@@ -28,31 +43,46 @@ const intervalDivPoints = setInterval(() => {
 
       const divTip = document.createElement("div");
       divTip.innerHTML = "Pontos recolhidos automaticamente";
-      divTip.classList.add("tw-tooltip", "tw-tooltip--align-center", "tw-tooltip--up");
-      divPoints.appendChild(divTip);   
+      divTip.classList.add(
+        "tw-tooltip",
+        "tw-tooltip--align-center",
+        "tw-tooltip--up"
+      );
+      divPoints.appendChild(divTip);
     }
-  };
+  }
 }, 500);
 
-const intervalPStreamer = setInterval(() =>{
-  const pStreamer = getElementByXpath("//p[@class='tw-c-text-inherit tw-font-size-5 tw-white-space-nowrap']");
+const intervalPStreamer = setInterval(() => {
+  const pStreamer = getElementByXpath(
+    "//p[@class='tw-c-text-inherit tw-font-size-5 tw-white-space-nowrap']"
+  );
   if (pStreamer) {
     compareStreamers(pStreamer.innerHTML);
-    getElementByXpath("//span[@id='span-number-points-receiver']").innerHTML = localStorage.getItem("gg_pointsReceivedToday");
+    getElementByXpath(
+      "//span[@id='span-number-points-receiver']"
+    ).innerHTML = localStorage.getItem("gg_pointsReceivedToday");
   }
 }, 3000);
 
-const intervalConfigTwitch = setInterval(() => {  
-  const divConfigTwitch = getElementByXpath("//div[@class='chat-settings__content tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-c-background-base tw-c-text-base tw-pd-1']");
+const intervalConfigTwitch = setInterval(() => {
+  const divConfigTwitch = getElementByXpath(
+    "//div[@class='chat-settings__content tw-border-bottom-left-radius-medium tw-border-bottom-right-radius-medium tw-c-background-base tw-c-text-base tw-pd-1']"
+  );
   if (divConfigTwitch) {
-    if(!getElementByXpath("//p[@id='GetGiftTitleId']")){
+    if (!getElementByXpath("//p[@id='GetGiftTitleId']")) {
       const divDivisor = document.createElement("div");
-      divDivisor.classList.add("tw-border-t", "tw-mg-t-1", "tw-mg-x-05", "tw-pd-b-1");
-      divConfigTwitch.appendChild(divDivisor);    
+      divDivisor.classList.add(
+        "tw-border-t",
+        "tw-mg-t-1",
+        "tw-mg-x-05",
+        "tw-pd-b-1"
+      );
+      divConfigTwitch.appendChild(divDivisor);
 
       const divConfig = document.createElement("div");
       divConfig.classList.add();
-      divConfigTwitch.appendChild(divConfig); 
+      divConfigTwitch.appendChild(divConfig);
 
       const divName = document.createElement("div");
       divName.classList.add("tw-mg-y-05", "tw-pd-x-05");
@@ -60,7 +90,12 @@ const intervalConfigTwitch = setInterval(() => {
 
       const pName = document.createElement("P");
       pName.id = "GetGiftTitleId";
-      pName.classList.add("tw-c-text-alt-2", "tw-font-size-6", "tw-strong", "tw-upcase");
+      pName.classList.add(
+        "tw-c-text-alt-2",
+        "tw-font-size-6",
+        "tw-strong",
+        "tw-upcase"
+      );
       pName.innerHTML = "Get Gift";
       divName.appendChild(pName);
 
@@ -73,7 +108,11 @@ const intervalConfigTwitch = setInterval(() => {
       divLabelSound.appendChild(divNameLabelSound);
 
       const labelSoundName = document.createElement("label");
-      labelSoundName.classList.add("tw-drop-down-menu-input-item__label", "tw-flex-grow-1", "tw-mg-r-2");
+      labelSoundName.classList.add(
+        "tw-drop-down-menu-input-item__label",
+        "tw-flex-grow-1",
+        "tw-mg-r-2"
+      );
       labelSoundName.innerHTML = "Habilitar/Desalitar som";
       labelSoundName.htmlFor = "gg-toggle-sound-name";
       divNameLabelSound.appendChild(labelSoundName);
@@ -81,23 +120,27 @@ const intervalConfigTwitch = setInterval(() => {
       const divLabelToggleSound = document.createElement("div");
       divLabelToggleSound.classList.add("tw-toggle");
       divNameLabelSound.appendChild(divLabelToggleSound);
-      
+
       const inputToggleSound = document.createElement("input");
       inputToggleSound.id = "gg-toggle-sound-name";
       inputToggleSound.type = "checkbox";
-      inputToggleSound.checked = localStorage.getItem("gg_soundCheck") == "true" ? true : false; 
+      inputToggleSound.checked =
+        localStorage.getItem("gg_soundCheck") == "true" ? true : false;
       inputToggleSound.classList.add("tw-toggle__input");
       divLabelToggleSound.appendChild(inputToggleSound);
-      setInterval(() =>{
+      setInterval(() => {
         if (inputToggleSound.checked) {
           localStorage.setItem("gg_soundCheck", true);
-        }else{
+        } else {
           localStorage.setItem("gg_soundCheck", false);
         }
       }, 100);
 
       const labelInputSoundName = document.createElement("label");
-      labelInputSoundName.classList.add("label-input-sound-name", "tw-toggle__button");
+      labelInputSoundName.classList.add(
+        "label-input-sound-name",
+        "tw-toggle__button"
+      );
       labelInputSoundName.htmlFor = "gg-toggle-sound-name";
       divLabelToggleSound.appendChild(labelInputSoundName);
 
@@ -109,32 +152,43 @@ const intervalConfigTwitch = setInterval(() => {
   }
 }, 500);
 
-
 let totalPointsReceived = 0;
 let valuePointReceived;
-const intervalClickButtonGift = setInterval(() =>{
-  const btnGift = getElementByXpath("//div[@class='claimable-bonus__icon tw-flex']");
-  if (!btnGift){
+const intervalClickButtonGift = setInterval(() => {
+  const btnGift = getElementByXpath(
+    "//div[@class='claimable-bonus__icon tw-flex']"
+  );
+  if (!btnGift) {
     return "Button Gift Not Find!";
   }
   btnGift.click();
   audioPlay(true);
   const intervalValuePointReceived = setInterval(() => {
-    valuePointReceived = getElementByXpath("//div[@class='tw-align-items-center tw-c-text-link tw-flex tw-pd-y-05']");
-    if (valuePointReceived){
+    valuePointReceived = getElementByXpath(
+      "//div[@class='tw-align-items-center tw-c-text-link tw-flex tw-pd-y-05']"
+    );
+    if (valuePointReceived) {
       clearInterval(intervalValuePointReceived);
-      totalPointsReceived = parseInt(localStorage.getItem("gg_pointsReceivedToday")) + parseInt(valuePointReceived.innerText.split("+")[1]);
+      totalPointsReceived =
+        parseInt(localStorage.getItem("gg_pointsReceivedToday")) +
+        parseInt(valuePointReceived.innerText.split("+")[1]);
       localStorage.setItem("gg_pointsReceivedToday", totalPointsReceived);
-      getElementByXpath("//span[@id='span-number-points-receiver']").innerHTML = localStorage.getItem("gg_pointsReceivedToday");
+      getElementByXpath(
+        "//span[@id='span-number-points-receiver']"
+      ).innerHTML = localStorage.getItem("gg_pointsReceivedToday");
     }
   }, 500);
 }, 5000);
 
-
 function compareDates() {
-  let date = new Date().getDate()+""+new Date().getMonth()+""+new Date().getFullYear();
-  
-  if(!(localStorage.getItem("gg_lastDate") == date)){
+  let date =
+    new Date().getDate() +
+    "" +
+    new Date().getMonth() +
+    "" +
+    new Date().getFullYear();
+
+  if (!(localStorage.getItem("gg_lastDate") == date)) {
     localStorage.setItem("gg_pointsReceivedToday", "00");
     localStorage.setItem("gg_lastDate", date);
   }
@@ -152,11 +206,36 @@ function compareStreamers(currentStreamer) {
 
 function audioPlay() {
   if (localStorage.getItem("gg_soundCheck") == "true") {
-    let audio = new Audio(chrome.runtime.getURL("assets/beep.mp3"));
+    switch (detectBrowser()) {
+      case "chrome":
+        var audio = new Audio(chrome.runtime.getURL("assets/beep.mp3"));
+        break;
+      case "mozilla":
+        var audio = new Audio(browser.runtime.getURL("assets/beep.mp3"));
+        break;
+    }
     audio.play();
   }
 }
 
+function detectBrowser() {
+  if (typeof InstallTrigger !== "undefined") {
+    return "mozilla";
+  }
+  if (
+    !!window.chrome &&
+    (!!window.chrome.webstore || !!window.chrome.runtime)
+  ) {
+    return "chrome";
+  }
+}
+
 function getElementByXpath(path) {
-  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  return document.evaluate(
+    path,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
 }
